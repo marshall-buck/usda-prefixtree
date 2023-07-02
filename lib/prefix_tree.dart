21a2a21ec37dc5f80b1df405d67002e29a01c639
Marshall Buck <dev@marshallbuck.com>
2023-07-02 16:09:38 -0500

    added insert test to Prefixtree also added serach to tre calss

--- lib/prefix_tree.dart
+++ lib/prefix_tree.dart
@@ -6,25 +6,12 @@ import 'package:usda_db_creation/helpers/file_helpers.dart';
 import 'prefix_node.dart';
 
 const wordIndexPath = './lib/db/word_index_db.json';
+const testWordList = ['ali', 'alice', 'anna', 'elias', 'eliza'];
 
 void main() async {
-  // final x = PrefixTree();
-  final indexDb = await readJsonFile(wordIndexPath);
-  final wordList = indexDb.keys.toList() as List<String>;
-
-  int middleIndex = (wordList.length / 2).round();
-  String removedElement = wordList.removeAt(middleIndex);
-  wordList.insert(0, removedElement);
-
-  // final wordList = ['ali', 'alice', 'anna', 'elias', 'eliza'];
-  // // final wordList = ['ali', 'alice', 'anna', 'elias', 'eliza'];
-
-  // x.insertWordList(wordList);
-
-  // print(x);
-
-  // writeJsonFile('lib/db/prefix_tree.json', x.toJson());
-//   print(x.search('app'));
+  // await createMainPrefixTree();
+  final tree = await createTestingPrefixTree();
+  print(tree.searchByPrefix('elia'));
 }
 
 class PrefixTree {
@@ -38,18 +25,18 @@ class PrefixTree {
   /// - [node] The node to be added. If null, a new node will be created.
   ///
   /// Returns the added node.
-  PTNode? addNode(String word, int pos, PTNode? node) {
+  static PTNode? _addNode(String word, int pos, PTNode? node) {
     node ??= PTNode(key: word[pos]);
 
     if (word.codeUnitAt(pos) < node.key!.codeUnitAt(0)) {
-      node.left = addNode(word, pos, node.left);
+      node.left = _addNode(word, pos, node.left);
     } else if (word.codeUnitAt(pos) > node.key!.codeUnitAt(0)) {
-      node.right = addNode(word, pos, node.right);
+      node.right = _addNode(word, pos, node.right);
     } else {
       if (pos + 1 == word.length) {
         node.isEnd = true;
       } else {
-        node.middle = addNode(word, pos + 1, node.middle);
+        node.middle = _addNode(word, pos + 1, node.middle);
       }
     }
 
@@ -60,8 +47,8 @@ class PrefixTree {
   ///
   /// Parameters:
   /// - [word] The word to be added.
-  void addWord(String word) {
-    root = addNode(word, 0, root);
+  void _addWord(String word) {
+    root = _addNode(word, 0, root);
   }
 
   /// Inserts a list of words into the prefix tree.
@@ -70,7 +57,7 @@ class PrefixTree {
   /// - [wordList] The list of words to be inserted.
   void insertWordList(List<String> wordList) {
     for (final word in wordList) {
-      addWord(word);
+      _addWord(word);
     }
   }
 
@@ -87,7 +74,7 @@ class PrefixTree {
   /// - [jsonFile]: The JSON file to read and parse.
   ///
   /// Returns a `PrefixTree` object created from the JSON data.
-  static PrefixTree fromJsonFile(File jsonFile) {
+  PrefixTree fromJsonFile(File jsonFile) {
     String jsonString = jsonFile.readAsStringSync();
     Map<String, dynamic> jsonMap = json.decode(jsonString);
     PrefixTree prefixTree = PrefixTree();
@@ -95,28 +82,119 @@ class PrefixTree {
     return prefixTree;
   }
 
+  /// Searches for all words starting with a given prefix.
+  ///
+  /// Parameters:
+  /// - [prefix]: The prefix to search for.
+  ///
+  /// Returns a list of strings that start with the prefix. If no matches are found, an empty list is returned.
+  List<String> searchByPrefix(String prefix) {
+    PTNode? node = findNode(prefix);
+    List<String> result = [];
+
+    if (node != null) {
+      if (node.isEnd == true) {
+        // If the prefix itself is a word, add it to the result
+        result.add(prefix);
+      }
+
+      // Collect all words starting from the node's subtree
+      _collectWords(node.middle, prefix, result);
+    }
+
+    return result;
+  }
+
+  /// Recursively collects all words from a given node and its subtree.
+  ///
+  /// Parameters:
+  /// - [node]: The current node to collect words from.
+  /// - [prefix]: The prefix used for searching.
+  /// - [result]: The list to collect the matched words.
+  static void _collectWords(PTNode? node, String prefix, List<String> result) {
+    if (node == null) {
+      return;
+    }
+
+    // Recursively collect words from the left, middle, and right nodes
+    _collectWords(node.left, prefix, result);
+
+    // If the current node is an end node, add it to the result list
+    if (node.isEnd == true) {
+      result.add(prefix + node.key!);
+    }
+
+    _collectWords(node.middle, prefix + node.key!, result);
+    _collectWords(node.right, prefix, result);
+  }
+
+  /// Finds the node that corresponds to the given prefix.
+  ///
+  /// Parameters:
+  /// - [prefix]: The prefix to search for.
+  ///
+  /// Returns the node that corresponds to the prefix, or null if not found.
+  PTNode? findNode(String prefix) {
+    PTNode? node = root;
+    int pos = 0;
+
+    while (node != null) {
+      if (prefix.codeUnitAt(pos) < node.key!.codeUnitAt(0)) {
+        node = node.left;
+      } else if (prefix.codeUnitAt(pos) > node.key!.codeUnitAt(0)) {
+        node = node.right;
+      } else {
+        pos++;
+
+        if (pos == prefix.length) {
+          // Reached the end of the prefix
+          return node;
+        }
+
+        node = node.middle;
+      }
+    }
+
+    return null;
+  }
+
   @override
   String toString() {
     return '$root';
   }
+}
 
-  // bool contains(String s) {
-  //   if (s == null || s.isEmpty) throw ArgumentError();
+/// FOR INITIAL POPULATION AND TESTING
 
-  //   int pos = 0;
-  //   Node? node = root;
-  //   while (node != null) {
-  //     int cmp = s.codeUnitAt(pos) - node.key.codeUnitAt(0);
-  //     if (s.codeUnitAt(pos) < node.key.codeUnitAt(0)) {
-  //       node = node.left;
-  //     } else if (s.codeUnitAt(pos) > node.key.codeUnitAt(0)) {
-  //       node = node.right;
-  //     } else {
-  //       if (++pos == s.length) return node.isEnd;
-  //       node = node.middle;
-  //     }
-  //   }
+Future<void> createMainPrefixTree() async {
+  final x = PrefixTree();
+  final indexDb = await readJsonFile(wordIndexPath);
+  final wordList = indexDb.keys.toList() as List<String>;
 
-  //   return false;
-  // }
+  int middleIndex = (wordList.length / 2).round();
+  String removedElement = wordList.removeAt(middleIndex);
+  wordList.insert(0, removedElement);
+
+  // final wordList = ['ali', 'alice', 'anna', 'elias', 'eliza'];
+
+  x.insertWordList(wordList);
+
+  writeJsonFile('lib/db/prefix_tree.json', x.toJson());
+}
+
+Future<PrefixTree> createTestingPrefixTree() async {
+  final x = PrefixTree();
+  // final indexDb = await readJsonFile(wordIndexPath);
+  // final wordList = indexDb.keys.toList() as List<String>;
+
+  // int middleIndex = (wordList.length / 2).round();
+  // String removedElement = wordList.removeAt(middleIndex);
+  // wordList.insert(0, removedElement);
+
+  // final wordList = ['ali', 'alice', 'anna', 'elias', 'eliza'];
+
+  x.insertWordList(testWordList);
+
+  writeJsonFile('lib/db/test_prefix_tree.json', x.toJson());
+  return x;
 }
--- test/prefix_tree_test.dart
+++ test/prefix_tree_test.dart
@@ -1,8 +1,142 @@
-// import 'package:test/test.dart';
-// import 'package:usda_db_creation/prefix_node.dart';
+import 'package:test/test.dart';
+import 'package:usda_db_creation/prefix_node.dart';
+import 'dart:convert';
+import 'dart:io';
 
-// void main() {
-//   group('PrefixTree', () {
-//     test('toJson should convert PTNode to a map', () {});
-//   });
-// }
+import 'package:usda_db_creation/prefix_tree.dart';
+
+void main() {
+  group('PrefixTree', () {
+    test('Inserts words correctly', () {
+      PrefixTree prefixTree = PrefixTree();
+      prefixTree.insertWordList(['ali', 'alice', 'anna', 'elias', 'eliza']);
+
+      expect(prefixTree.root!.key, 'a');
+      expect(prefixTree.root!.middle!.key, 'l');
+      expect(prefixTree.root!.middle!.right!.key, 'n');
+      expect(prefixTree.root!.middle!.right!.middle!.key, 'n');
+      expect(prefixTree.root!.middle!.right!.middle!.middle!.key, 'a');
+      expect(prefixTree.root!.middle!.right!.middle!.middle!.isEnd, true);
+      expect(prefixTree.root!.middle!.middle!.key, 'i');
+      expect(prefixTree.root!.middle!.middle!.isEnd, true);
+      expect(prefixTree.root!.middle!.middle!.middle!.key, 'c');
+      expect(prefixTree.root!.middle!.middle!.middle!.middle!.key, 'e');
+      expect(prefixTree.root!.middle!.middle!.middle!.middle!.isEnd, true);
+
+      expect(prefixTree.root!.right!.key, 'e');
+      expect(prefixTree.root!.right!.middle!.key, 'l');
+      expect(prefixTree.root!.right!.middle!.middle!.key, 'i');
+      expect(prefixTree.root!.right!.middle!.middle!.middle!.key, 'a');
+      expect(prefixTree.root!.right!.middle!.middle!.middle!.middle!.key, 's');
+      expect(
+          prefixTree.root!.right!.middle!.middle!.middle!.middle!.isEnd, true);
+
+      expect(prefixTree.root!.right!.middle!.middle!.middle!.right!.key, 'z');
+      expect(prefixTree.root!.right!.middle!.middle!.middle!.right!.middle!.key,
+          'a');
+      expect(
+          prefixTree.root!.right!.middle!.middle!.middle!.right!.middle!.isEnd,
+          true);
+
+      // expect(prefixTree.root!.middle!.middle!.right!.key, 'n');
+      // expect(prefixTree.root!.middle!.middle!.right!.right!.key, 'n');
+      // expect(prefixTree.root!.middle!.middle!.right!.right!.isEnd, true);
+
+      // expect(prefixTree.root!.right!.key, 'e');
+      // expect(prefixTree.root!.right!.middle!.key, 'l');
+      // expect(prefixTree.root!.right!.middle!.middle!.key, 'i');
+      // expect(prefixTree.root!.right!.middle!.middle!.middle!.key, 'a');
+      // expect(prefixTree.root!.right!.middle!.middle!.middle!.isEnd, true);
+
+      // expect(prefixTree.root!.right!.middle!.right!.key, 'z');
+      // expect(prefixTree.root!.right!.middle!.right!.middle!.key, 'a');
+      // expect(prefixTree.root!.right!.middle!.right!.middle!.isEnd, true);
+    });
+
+    test('Converts to JSON correctly', () {
+      PrefixTree prefixTree = PrefixTree();
+      prefixTree.insertWordList(['ali', 'alice', 'anna', 'elias', 'eliza']);
+
+      Map<String, dynamic> json = prefixTree.toJson();
+      String jsonString = jsonEncode(json);
+
+      expect(jsonString,
+          '{"root":{"key":"a","middle":{"key":"l","middle":{"key":"i","isEnd":true},"right":{"key":"c","middle":{"key":"e","isEnd":true}},"right":{"key":"n","right":{"key":"n","isEnd":true}}}},"right":{"key":"e","middle":{"key":"l","middle":{"key":"i","middle":{"key":"a","isEnd":true}},"right":{"key":"z","middle":{"key":"a","isEnd":true}}}}}}}');
+    });
+
+    test('Creates PrefixTree object from JSON file', () {
+      PrefixTree prefixTree = PrefixTree();
+      prefixTree.insertWordList(['ali', 'alice', 'anna', 'elias', 'eliza']);
+
+      // Create a temporary JSON file
+      File jsonFile = File('temp.json');
+      jsonFile.writeAsStringSync(
+          '{"root":{"key":"a","middle":{"key":"l","middle":{"key":"i","isEnd":true},"right":{"key":"c","middle":{"key":"e","isEnd":true}},"right":{"key":"n","right":{"key":"n","isEnd":true}}}},"right":{"key":"e","middle":{"key":"l","middle":{"key":"i","middle":{"key":"a","isEnd":true}},"right":{"key":"z","middle":{"key":"a","isEnd":true}}}}}}}');
+
+      PrefixTree loadedPrefixTree = PrefixTree().fromJsonFile(jsonFile);
+
+      expect(loadedPrefixTree.root!.key, 'a');
+      expect(loadedPrefixTree.root!.middle!.key, 'l');
+      expect(loadedPrefixTree.root!.middle!.middle!.key, 'i');
+      expect(loadedPrefixTree.root!.middle!.middle!.isEnd, true);
+
+      expect(loadedPrefixTree.root!.middle!.right!.key, 'c');
+      expect(loadedPrefixTree.root!.middle!.right!.middle!.key, 'e');
+      expect(loadedPrefixTree.root!.middle!.right!.middle!.isEnd, true);
+
+      expect(loadedPrefixTree.root!.middle!.middle!.right!.key, 'n');
+      expect(loadedPrefixTree.root!.middle!.middle!.right!.right!.key, 'n');
+      expect(loadedPrefixTree.root!.middle!.middle!.right!.right!.isEnd, true);
+
+      expect(loadedPrefixTree.root!.right!.key, 'e');
+      expect(loadedPrefixTree.root!.right!.middle!.key, 'l');
+      expect(loadedPrefixTree.root!.right!.middle!.middle!.key, 'i');
+      expect(loadedPrefixTree.root!.right!.middle!.middle!.middle!.key, 'a');
+      expect(loadedPrefixTree.root!.right!.middle!.middle!.middle!.isEnd, true);
+
+      expect(loadedPrefixTree.root!.right!.middle!.right!.key, 'z');
+      expect(loadedPrefixTree.root!.right!.middle!.right!.middle!.key, 'a');
+      expect(loadedPrefixTree.root!.right!.middle!.right!.middle!.isEnd, true);
+
+      // Delete the temporary JSON file
+      jsonFile.deleteSync();
+    });
+
+    test('Searches words by prefix correctly', () {
+      PrefixTree prefixTree = PrefixTree();
+      prefixTree.insertWordList(['ali', 'alice', 'anna', 'elias', 'eliza']);
+
+      expect(prefixTree.searchByPrefix('al'), ['ali', 'alice', 'anna']);
+      expect(prefixTree.searchByPrefix('el'), ['elias', 'eliza']);
+      expect(prefixTree.searchByPrefix('elis'), ['elisa']);
+      expect(prefixTree.searchByPrefix('anna'), []);
+      expect(prefixTree.searchByPrefix('z'), []);
+    });
+
+    test('Finds node for prefix correctly', () {
+      PrefixTree prefixTree = PrefixTree();
+      prefixTree.insertWordList(['ali', 'alice', 'anna', 'elias', 'eliza']);
+
+      PTNode? node1 = prefixTree.findNode('al');
+      expect(node1!.key, 'l');
+      expect(node1.middle!.key, 'i');
+      expect(node1.middle!.isEnd, true);
+
+      PTNode? node2 = prefixTree.findNode('el');
+      expect(node2!.key, 'l');
+      expect(node2.middle!.key, 'i');
+      expect(node2.middle!.middle!.key, 'a');
+      expect(node2.middle!.middle!.isEnd, true);
+
+      PTNode? node3 = prefixTree.findNode('elis');
+      expect(node3!.key, 's');
+      expect(node3.isEnd, true);
+
+      PTNode? node4 = prefixTree.findNode('anna');
+      expect(node4, null);
+
+      PTNode? node5 = prefixTree.findNode('z');
+      expect(node5, null);
+    });
+  });
+}
