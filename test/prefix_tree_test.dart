78c4d78845ed9475fc4b664ed260cb27fe3436dd
Marshall Buck <dev@marshallbuck.com>
2023-07-02 16:30:08 -0500

    added test_prefix_tree.json, addded search tests for tree

--- test/prefix_tree_test.dart
+++ test/prefix_tree_test.dart
@@ -37,20 +37,6 @@ void main() {
       expect(
           prefixTree.root!.right!.middle!.middle!.middle!.right!.middle!.isEnd,
           true);
-
-      // expect(prefixTree.root!.middle!.middle!.right!.key, 'n');
-      // expect(prefixTree.root!.middle!.middle!.right!.right!.key, 'n');
-      // expect(prefixTree.root!.middle!.middle!.right!.right!.isEnd, true);
-
-      // expect(prefixTree.root!.right!.key, 'e');
-      // expect(prefixTree.root!.right!.middle!.key, 'l');
-      // expect(prefixTree.root!.right!.middle!.middle!.key, 'i');
-      // expect(prefixTree.root!.right!.middle!.middle!.middle!.key, 'a');
-      // expect(prefixTree.root!.right!.middle!.middle!.middle!.isEnd, true);
-
-      // expect(prefixTree.root!.right!.middle!.right!.key, 'z');
-      // expect(prefixTree.root!.right!.middle!.right!.middle!.key, 'a');
-      // expect(prefixTree.root!.right!.middle!.right!.middle!.isEnd, true);
     });
 
     test('Converts to JSON correctly', () {
@@ -61,61 +47,57 @@ void main() {
       String jsonString = jsonEncode(json);
 
       expect(jsonString,
-          '{"root":{"key":"a","middle":{"key":"l","middle":{"key":"i","isEnd":true},"right":{"key":"c","middle":{"key":"e","isEnd":true}},"right":{"key":"n","right":{"key":"n","isEnd":true}}}},"right":{"key":"e","middle":{"key":"l","middle":{"key":"i","middle":{"key":"a","isEnd":true}},"right":{"key":"z","middle":{"key":"a","isEnd":true}}}}}}}');
+          '{"root":{"key":"a","isEnd":false,"left":null,"middle":{"key":"l","isEnd":false,"left":null,"middle":{"key":"i","isEnd":true,"left":null,"middle":{"key":"c","isEnd":false,"left":null,"middle":{"key":"e","isEnd":true,"left":null,"middle":null,"right":null},"right":null},"right":null},"right":{"key":"n","isEnd":false,"left":null,"middle":{"key":"n","isEnd":false,"left":null,"middle":{"key":"a","isEnd":true,"left":null,"middle":null,"right":null},"right":null},"right":null}},"right":{"key":"e","isEnd":false,"left":null,"middle":{"key":"l","isEnd":false,"left":null,"middle":{"key":"i","isEnd":false,"left":null,"middle":{"key":"a","isEnd":false,"left":null,"middle":{"key":"s","isEnd":true,"left":null,"middle":null,"right":null},"right":{"key":"z","isEnd":false,"left":null,"middle":{"key":"a","isEnd":true,"left":null,"middle":null,"right":null},"right":null}},"right":null},"right":null},"right":null}}}');
     });
 
-    test('Creates PrefixTree object from JSON file', () {
-      PrefixTree prefixTree = PrefixTree();
-      prefixTree.insertWordList(['ali', 'alice', 'anna', 'elias', 'eliza']);
+    test('Creates PrefixTree object from JSON file', () async {
+      File jsonFile = File('./test/data/test_prefix_tree.json');
 
-      // Create a temporary JSON file
-      File jsonFile = File('temp.json');
-      jsonFile.writeAsStringSync(
-          '{"root":{"key":"a","middle":{"key":"l","middle":{"key":"i","isEnd":true},"right":{"key":"c","middle":{"key":"e","isEnd":true}},"right":{"key":"n","right":{"key":"n","isEnd":true}}}},"right":{"key":"e","middle":{"key":"l","middle":{"key":"i","middle":{"key":"a","isEnd":true}},"right":{"key":"z","middle":{"key":"a","isEnd":true}}}}}}}');
+      PrefixTree prefixTree = PrefixTree().fromJsonFile(jsonFile);
 
-      PrefixTree loadedPrefixTree = PrefixTree().fromJsonFile(jsonFile);
+      expect(prefixTree.root!.key, 'a');
+      expect(prefixTree.root!.middle!.key, 'l');
+      expect(prefixTree.root!.middle!.right!.key, 'n');
+      expect(prefixTree.root!.middle!.right!.middle!.key, 'n');
+      expect(prefixTree.root!.middle!.right!.middle!.middle!.key, 'a');
+      expect(prefixTree.root!.middle!.right!.middle!.middle!.isEnd, true);
+      expect(prefixTree.root!.middle!.middle!.key, 'i');
+      expect(prefixTree.root!.middle!.middle!.isEnd, true);
+      expect(prefixTree.root!.middle!.middle!.middle!.key, 'c');
+      expect(prefixTree.root!.middle!.middle!.middle!.middle!.key, 'e');
+      expect(prefixTree.root!.middle!.middle!.middle!.middle!.isEnd, true);
 
-      expect(loadedPrefixTree.root!.key, 'a');
-      expect(loadedPrefixTree.root!.middle!.key, 'l');
-      expect(loadedPrefixTree.root!.middle!.middle!.key, 'i');
-      expect(loadedPrefixTree.root!.middle!.middle!.isEnd, true);
+      expect(prefixTree.root!.right!.key, 'e');
+      expect(prefixTree.root!.right!.middle!.key, 'l');
+      expect(prefixTree.root!.right!.middle!.middle!.key, 'i');
+      expect(prefixTree.root!.right!.middle!.middle!.middle!.key, 'a');
+      expect(prefixTree.root!.right!.middle!.middle!.middle!.middle!.key, 's');
+      expect(
+          prefixTree.root!.right!.middle!.middle!.middle!.middle!.isEnd, true);
 
-      expect(loadedPrefixTree.root!.middle!.right!.key, 'c');
-      expect(loadedPrefixTree.root!.middle!.right!.middle!.key, 'e');
-      expect(loadedPrefixTree.root!.middle!.right!.middle!.isEnd, true);
-
-      expect(loadedPrefixTree.root!.middle!.middle!.right!.key, 'n');
-      expect(loadedPrefixTree.root!.middle!.middle!.right!.right!.key, 'n');
-      expect(loadedPrefixTree.root!.middle!.middle!.right!.right!.isEnd, true);
-
-      expect(loadedPrefixTree.root!.right!.key, 'e');
-      expect(loadedPrefixTree.root!.right!.middle!.key, 'l');
-      expect(loadedPrefixTree.root!.right!.middle!.middle!.key, 'i');
-      expect(loadedPrefixTree.root!.right!.middle!.middle!.middle!.key, 'a');
-      expect(loadedPrefixTree.root!.right!.middle!.middle!.middle!.isEnd, true);
-
-      expect(loadedPrefixTree.root!.right!.middle!.right!.key, 'z');
-      expect(loadedPrefixTree.root!.right!.middle!.right!.middle!.key, 'a');
-      expect(loadedPrefixTree.root!.right!.middle!.right!.middle!.isEnd, true);
-
-      // Delete the temporary JSON file
-      jsonFile.deleteSync();
+      expect(prefixTree.root!.right!.middle!.middle!.middle!.right!.key, 'z');
+      expect(prefixTree.root!.right!.middle!.middle!.middle!.right!.middle!.key,
+          'a');
+      expect(
+          prefixTree.root!.right!.middle!.middle!.middle!.right!.middle!.isEnd,
+          true);
     });
 
     test('Searches words by prefix correctly', () {
       PrefixTree prefixTree = PrefixTree();
       prefixTree.insertWordList(['ali', 'alice', 'anna', 'elias', 'eliza']);
 
-      expect(prefixTree.searchByPrefix('al'), ['ali', 'alice', 'anna']);
+      expect(prefixTree.searchByPrefix('al'), ['ali', 'alice']);
       expect(prefixTree.searchByPrefix('el'), ['elias', 'eliza']);
-      expect(prefixTree.searchByPrefix('elis'), ['elisa']);
-      expect(prefixTree.searchByPrefix('anna'), []);
+      expect(prefixTree.searchByPrefix('elis'), []);
+      expect(prefixTree.searchByPrefix('anna'), ['anna']);
       expect(prefixTree.searchByPrefix('z'), []);
     });
 
     test('Finds node for prefix correctly', () {
-      PrefixTree prefixTree = PrefixTree();
-      prefixTree.insertWordList(['ali', 'alice', 'anna', 'elias', 'eliza']);
+      File jsonFile = File('./test/data/test_prefix_tree.json');
+
+      PrefixTree prefixTree = PrefixTree().fromJsonFile(jsonFile);
 
       PTNode? node1 = prefixTree.findNode('al');
       expect(node1!.key, 'l');
@@ -126,14 +108,13 @@ void main() {
       expect(node2!.key, 'l');
       expect(node2.middle!.key, 'i');
       expect(node2.middle!.middle!.key, 'a');
-      expect(node2.middle!.middle!.isEnd, true);
+      expect(node2.middle!.middle!.middle!.isEnd, true);
 
       PTNode? node3 = prefixTree.findNode('elis');
-      expect(node3!.key, 's');
-      expect(node3.isEnd, true);
+      expect(node3, isNull);
 
       PTNode? node4 = prefixTree.findNode('anna');
-      expect(node4, null);
+      expect(node4!.isEnd, true);
 
       PTNode? node5 = prefixTree.findNode('z');
       expect(node5, null);
