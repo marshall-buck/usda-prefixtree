a8a884303e73330ade837d647cb01e6d43c79ff6
Marshall Buck <dev@marshallbuck.com>
2023-12-25 16:30:04 -0600

    Rename DBParser class prop to originalFoodsList

--- lib/db_parser.dart
+++ lib/db_parser.dart
@@ -4,10 +4,10 @@ import 'package:usda_db_creation/file_loader_service.dart';
 
 class DBParser {
   FileLoaderService fileLoader;
-  Map<dynamic, dynamic>? _dbMap;
+  Map<dynamic, dynamic>? _originalDBMap;
 
   /// [List] of foods from the database.
-  List<dynamic> get foodsDBMap => _dbMap?['SRLegacyFoods'];
+  List<dynamic> get originalFoodsList => _originalDBMap?['SRLegacyFoods'];
 
   DBParser({final FileLoaderService? fileLoader})
       : fileLoader = fileLoader ?? FileLoaderService();
@@ -15,13 +15,13 @@ class DBParser {
   /// Populates the _dbMap
   void init(final String path) {
     final file = fileLoader.loadData(path);
-    _dbMap = jsonDecode(file);
+    _originalDBMap = jsonDecode(file);
   }
 
   (Map<String, int>, int) getFoodCategories() {
     final Map<String, int> categories = {};
     int count = 0;
-    for (final food in foodsDBMap) {
+    for (final food in originalFoodsList) {
       final obj = food['foodCategory'];
       final cat = obj['description'];
       if (categories.containsKey(cat)) {
@@ -36,3 +36,29 @@ class DBParser {
     return (categories, count);
   }
 }
+// ({Baked Products: 517,
+//Snacks: 176,
+//Sweets: 358,
+//Vegetables and Vegetable Products: 814,
+//American Indian/Alaska Native Foods: 165,
+//Restaurant Foods: 109,
+//Beverages: 366,
+//Fats and Oils: 216,
+//Sausages and Luncheon Meats: 167,
+//Dairy and Egg Products: 291,
+//Baby Foods: 345,
+//Poultry Products: 383,
+//Pork Products: 336,
+//Breakfast Cereals: 195,
+//Legumes and Legume Products: 290,
+//Finfish and Shellfish Products: 264,
+//Fruits and Fruit Juices: 355,
+//Cereal Grains and Pasta: 181,
+//Nut and Seed Products: 137,
+//Beef Products: 954,
+//Meals, Entrees, and Side Dishes: 81,
+//Fast Foods: 312,
+//Spices and Herbs: 63,
+//Soups, Sauces, and Gravies: 254,
+//Lamb, Veal, and Game Products: 464},
+// 7793)
