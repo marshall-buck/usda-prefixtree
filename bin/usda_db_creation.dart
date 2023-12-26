a000140e954f32a1d5813a0a1e98b560cb2d8d48
Marshall Buck <dev@marshallbuck.com>
2023-12-26 15:16:40 -0600

    changed barrel filename to db_creation

--- bin/usda_db_creation.dart
+++ bin/usda_db_creation.dart
@@ -1,6 +1,7 @@
 import 'package:usda_db_creation/db_parser.dart';
+import 'package:usda_db_creation/description_parser.dart';
 import 'package:usda_db_creation/file_loader_service.dart';
-import 'package:usda_db_creation/usda_db_creation.dart' as usda_db_creation;
+import 'package:usda_db_creation/db_creation.dart' as db;
 
 // import '../test/create_word_index_test.dart';
 
@@ -11,6 +12,10 @@ void main() async {
   final fileLoader = FileLoaderService();
   final dbParser = DBParser(fileLoader: fileLoader);
   dbParser.init(relativeOriginalDBPath);
+  final list = DescriptionParser.createOriginalDescriptionRecords(
+      originalFoodsList: dbParser.originalFoodsList);
+  print(list.length);
+
   // usda_db_creation.writeOriginalDescriptionsToFile(
   //     dbParser: dbParser, fileLoaderService: fileLoader);
   // usda_db_creation.writeDuplicatePhrasesToFile(
