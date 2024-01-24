9f4bdaae793a77626c29640f64007bb0bee33d31
Marshall Buck <dev@marshallbuck.com>
2024-01-24 17:14:10 -0600

    Refactor DataStructure class docs

--- lib/data_structure.dart
+++ lib/data_structure.dart
@@ -2,10 +2,9 @@ import 'package:usda_db_creation/db_parser.dart';
 
 /// Abstract class for the database data structures.
 /// All classes that create Objects for the Db must implement this method.
-/// Parameters:
 /// [dbParser] - The instance of the  [DBParser] class.
-/// [returnStructure] - If true the method will return the object created.
-/// [writeFile] - If true will write the data to a file.
+/// [returnStructure] - Whether to return the object created.
+/// [writeFile] - Whether to write the data to a file.
 abstract class DataStructure<T> {
   Future<T> createDataStructure({
     required DBParser dbParser,
